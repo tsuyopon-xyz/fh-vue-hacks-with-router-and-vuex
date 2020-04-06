03ee48156839c8ddb28c9d021c8657ccb6679fa2
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2020-04-06 13:48:52 +0900

    Modifid db config.

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC150PxS8rIAtk40ypRLRrVM5yxwkMHM9o",
-  authDomain: "vue-hacks2.firebaseapp.com",
-  databaseURL: "https://vue-hacks2.firebaseio.com",
-  projectId: "vue-hacks2",
-  storageBucket: "vue-hacks2.appspot.com",
-  messagingSenderId: "1088920753577",
-  appId: "1:1088920753577:web:53c963f14cd4da0cd6e0b8",
-  measurementId: "G-VWWKTVM3LR"
+  apiKey: "AIzaSyBFEFd1iYiy0r_x6l3Ldao3HOcFT86VB3A",
+  authDomain: "vue-hacks-b1415.firebaseapp.com",
+  databaseURL: "https://vue-hacks-b1415.firebaseio.com",
+  projectId: "vue-hacks-b1415",
+  storageBucket: "vue-hacks-b1415.appspot.com",
+  messagingSenderId: "681190762344",
+  appId: "1:681190762344:web:ea244d511029f1395258dd",
+  measurementId: "G-JYBY9W56GE"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
